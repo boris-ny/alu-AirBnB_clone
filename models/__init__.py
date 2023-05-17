0b612af9f986ea2f00c4278ec2d78e40c4c618d3
boris-ny <b.nyilindek@alustudent.com>
2023-05-17 17:51:13 +0200

    Base modules

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is the init file for the models package"""
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""This module is for the FileStorage class"""
+import json
+import os
+import datetime
+
+class FileStorage():
+    """
+    Class for storing and retrieving data
+    
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: dictionary to store objects
+    
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = obj.__class__.__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file"""
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w',encoding="utf-8") as f:
+            json.dump(new_dict, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(FileStorage.__file_path, 'r',encoding="utf-8") as f:
+                new_dict = json.load(f)
+            for key, value in new_dict.items():
+                new_dict[key] = eval(value["__class__"])(**value)
+            FileStorage.__objects = new_dict
+        except FileNotFoundError:
+            pass
