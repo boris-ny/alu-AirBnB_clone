718558cce792434242ed9ca1dd166e5f3de3c9f9
makuachteny <m.gatluak@alustudent.com>
2023-05-23 23:44:37 +0300

    corrected the conflict

--- models/city.py
+++ models/city.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #!/usr/bin/python3
 """This module creates a city class"""
-
-=======
->>>>>>> 3d797718ce7e50dc59d13958c124892886e6ecbb
-=======
->>>>>>> 9d13ffd5ceb620a4ee8d857f99814e12541b88d5
 from models.base_model import BaseModel
 
 
@@ -15,16 +8,7 @@ class City(BaseModel):
 
     state_id = ""
     name = ""
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 9d13ffd5ceb620a4ee8d857f99814e12541b88d5
 
     def __init__(self, *args, **kwargs):
         """Instantiates a City object"""
         super().__init__(*args, **kwargs)
-<<<<<<< HEAD
->>>>>>> 3d797718ce7e50dc59d13958c124892886e6ecbb
-=======
->>>>>>> 9d13ffd5ceb620a4ee8d857f99814e12541b88d5
