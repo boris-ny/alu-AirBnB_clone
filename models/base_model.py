3b352537af1823ee49accf4b755e629dbdad2c2e
makuachteny <m.gatluak@alustudent.com>
2023-05-24 00:30:37 +0300

    Corrected a typo

--- models/base_model.py
+++ models/base_model.py
@@ -5,8 +5,10 @@ import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
     """A base class for all models"""
+
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel."""
         if kwargs:
@@ -25,20 +27,18 @@ class BaseModel:
 
     def __str__(self):
         """Return a string representation of this BaseModel instance."""
-        cls = (str(type(self)).split(".")[-1]).split("\'")[0]
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
-    
+        cls = type(self).__name__
+        return "[{}] ({}) {}".format(cls, self.id, self.__dict__)
+
     def save(self):
-        """Update updated_at with the current datetime."""
+        """Update updated_at with the current datetime and save the object"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """Return a dictionary representation of this BaseModel instance."""
-        dictionary= {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':(str(type(self)).split(".")[-1]).split("\'")[0]})
+        dictionary = dict(self.__dict__)
+        dictionary['__class__'] = type(self).__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
