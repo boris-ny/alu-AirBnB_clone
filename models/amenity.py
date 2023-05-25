cdb4c2b3238dffc9845aee21a9f88656d8dcb171
boris-ny <b.nyilindek@alustudent.com>
2023-05-25 11:11:15 +0200

    fixing bugs

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-"""This module defines a class Amenity"""
-from models.base_model import BaseModel,Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-from models.place import place_amenity
+"""Amenity class"""
+from models.base_model import BaseModel
 
-class Amenity(BaseModel,Base):
-    """
-    Represents an Amenity for a MySQL database.
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+
+class Amenity(BaseModel):
+    name = ""
+    """initialise class"""
+
     def __init__(self, *args, **kwargs):
-        """
-        Initializes an Amenity.
-        """
         super().__init__(*args, **kwargs)
